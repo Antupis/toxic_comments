39917e2df9537bf3e06a1e108cfc4f2a40ad7c85
Antti Tupamäki <antupis@gmail.com>
2018-02-07 18:45:04 +0200

    deleted unused comments

--- analysis.py
+++ analysis.py
@@ -1,15 +1,5 @@
-# This Python 3 environment comes with many helpful analytics libraries installed
-# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load in 
-
-import numpy as np  # linear algebra
-import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
-# Input data files are available in the "../input/" directory.
-# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
-
-
-# Any results you write to the current directory are saved as output.
-
+import numpy as np
+import pandas as pd
 from keras.preprocessing import text, sequence
 from torch import nn, optim
 from torch.autograd import Variable
